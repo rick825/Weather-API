487e4b204340ca9db386d236165b70b994eecb39
Kumar Das Akash <rickak825@gmail.com>
2024-04-27 20:46:52 +0530

    Updated Toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom';
 import App from './App.jsx';
 import './css/index.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const isMobile = window.innerWidth < 768; 
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <ToastContainer />
+    <ToastContainer position={isMobile ? "bottom-center" : "top-right"} />
   </React.StrictMode>
 );
